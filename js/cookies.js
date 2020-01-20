22e56597f1c70f9c0c96af889628992b7d5f3197
ruben.gonzalez <ruben.gonzalez@mkdautomotive.com>
2020-01-20 22:23:16 +0100

    Auditoria Tecnoderecho sobre politica de cookies

---
+++ js/cookies.js
@@ -0,0 +1,14 @@
+if (!sessionStorage.cookiesResponse){
+    $("#con-cookies").fadeIn("slow");
+}else{
+    $("#con-cookies").hide();
+}
+
+$("#btn-cookies-yes").click(function(){
+    sessionStorage.cookiesResponse=true;
+    $("#con-cookies").fadeOut("slow");
+});
+
+$("#btn-cookies-no").click(function(){
+    window.location="aviso.html";
+});
--- sendemail.php
+++ sendemail.php
@@ -1,23 +1,5 @@
 <?php
 
-	// header('Content-type: application/json');
-	// $status = array(
-		// 'type'=>'success',
-		// 'message'=>'Gracias por contactar con nosotros '
-	// );
-
-    // $name = @trim(stripslashes($_POST['name'])); 
-    // $email = @trim(stripslashes($_POST['email'])); 
-    // $subject = @trim(stripslashes($_POST['subject'])); 
-    // $phone = @trim(stripslashes($_POST['phone'])); 
-    // $message = @trim(stripslashes($_POST['message'])); 
-
-    // $email_from = $email;
-    // $email_to = 'info@servibusclima.com';
-
-    // $body = 'Name: ' . $_POST['name'] . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message ."\n\n Telefono: ".$phone;		
-    // $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
-
 	$name = trim(stripslashes($_POST['name'])); 
     $email = trim(stripslashes($_POST['email'])); 
     $subject = trim(stripslashes($_POST['subject'])); 
@@ -29,10 +11,7 @@
 
     $body = 'Nombre: '.$name."\n\n".'E-mail: '.$email."\n\n".'Teléfono: '.$phone."\n\n".'Mensaje: '.$message."\n\n";		
     $success = mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
-    
-	// echo json_encode($status);
-    // die;
 	
-	header('Location: contacto.php');
+	header('Location: contacto.html');
 	
 ?>
